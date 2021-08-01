454cd39c5f8d3b06ebdd99424ae4fa234e4c4aa0
wiameaAB <41843028+wiameaAB@users.noreply.github.com>
2021-08-01 22:23:32 +0200

    Add files via upload

---
+++ table de multiplication.py
@@ -0,0 +1,14 @@
+
+n = int(input("Entrer un nombre: "))
+np = -1
+while np < 10:
+    np += 1
+    produit = np*n
+    print(n,"x",np,"=",produit)
+print("thanks for using my program")
+
+
+
+
+
+    
